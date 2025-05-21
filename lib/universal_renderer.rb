50726b3459f84685fa631aa5570aded0e775aed8
thaske <10328778+thaske@users.noreply.github.com>
2025-05-20 23:06:33 -0400

    Update universal_renderer.rb

--- lib/universal_renderer.rb
+++ lib/universal_renderer.rb
@@ -1,11 +1,15 @@
 require "universal_renderer/version"
 require "universal_renderer/engine"
 require "universal_renderer/configuration"
-require "universal_renderer/ssr/scrubber"
+
+require "universal_renderer/renderable"
+
 require "universal_renderer/client/base"
 require "universal_renderer/client/stream"
-require "universal_renderer/rendering"
+
 require "universal_renderer/ssr/helpers"
+require "universal_renderer/ssr/placeholders"
+require "universal_renderer/ssr/scrubber"
 
 module UniversalRenderer
   class << self
