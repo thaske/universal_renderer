97a5a841b45f9b2e012d537e817a4711271ef34e
thaske <10328778+thaske@users.noreply.github.com>
2026-07-09 19:31:58 -0400

    Fix Rails engine load order

--- lib/universal_renderer.rb
+++ lib/universal_renderer.rb
@@ -1,15 +1,14 @@
 require "universal_renderer/version"
-require "universal_renderer/engine"
 require "universal_renderer/configuration"
-
+require "universal_renderer/ssr/helpers"
 require "universal_renderer/renderable"
+require "universal_renderer/engine"
 
 require "universal_renderer/ssr/response"
 
 require "universal_renderer/client/base"
 require "universal_renderer/client/stream"
 
-require "universal_renderer/ssr/helpers"
 require "universal_renderer/ssr/placeholders"
 require "universal_renderer/ssr/scrubber"
 
